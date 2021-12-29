f815767a764386c432c2dab97ad537bbafbf0c04
RFilho01 <robertofilho0100@outlook.com>
2021-12-29 19:20:57 -0300

    validando tela de login

--- features/support/screens/components.rb
+++ features/support/screens/components.rb
@@ -4,9 +4,13 @@ class Navigator
         hamburger = "//android.widget.ImageButton[@content-desc='Open navigation drawer']"
         find_element(xpath: hamburger).click
     end
-    
+
     def list
         return find_element(id: "io.qaninja.android.twp:id/navView") # nao podemos fazer validacoes nessa camada de screen  object, apenas mapear os elementos e dar funcionalidade
     
+    end
+
+    def tap_by_text(target)
+        find_element(xpath: "//*[@text='#{target}']").click
     end
 end
